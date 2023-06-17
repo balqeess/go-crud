7fb27f8a9e1d05f69dddcdc9b1e98ff88231dae6
Balqees Al Jabri <balqeesaljabri88@gmail.com>
2023-06-17 16:14:35 +0400

    perform automatic database migration

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/balqees/go-crud/initializers"
+	"github.com/balqees/go-crud/models"
 )
 
 func init() {
 	initializers.LoadEnvVariables()
 	initializers.ConnectToDB()
+}
+func main() {
+	initializers.DB.AutoMigrate(&models.User{})
+
 }
 
