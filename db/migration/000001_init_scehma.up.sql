74d9f5a3d9b82310f4631db54f54c390fd176904
Balqees Al Jabri <balqeesaljabri88@gmail.com>
2023-07-07 23:19:23 +0400

    merge ftStatistics into dockerft

--- db/migration/000001_init_scehma.up.sql
+++ db/migration/000001_init_scehma.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    created_at TIMESTAMPTZ DEFAULT current_timestamp,
-    updated_at TIMESTAMPTZ DEFAULT current_timestamp,
-    deleted_at TIMESTAMPTZ,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR(255)
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    date_of_birth TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    deleted_at TIMESTAMP
 );
